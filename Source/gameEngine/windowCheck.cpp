41b4183c57cb9c374d1e8670c75116492a6e9ec6
Ema <emanuela.sklarzyk@gmail.com>
2018-09-17 16:00:04 -0400

    Added window check
    added and updated main .cpp to run checks

---
+++ Source/gameEngine/windowCheck.cpp
@@ -0,0 +1,23 @@
+#include "windowCheck.h"
+#include <Windows.h>
+#include <iostream>
+
+
+using namespace std;
+
+
+
+void CheckWindow::openWindow() 
+{
+	
+	HWINSTA winLink = GetProcessWindowStation(); //gets handle to current window station
+	
+	if (winLink != NULL)
+	{
+		cout << "There is an instance of the widnow running" << endl;
+	}
+	else
+	{
+		cout << "there is no instance of the window running so techinically you shouldn't be able to see this message" << endl;
+	}
+}
